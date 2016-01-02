21aef83fcbef267c9206457a78ec016943fcc6d5
juberr <juberr1@live.ca>
2016-01-01 23:24:34 -0500

    fixed spawns

--- round3/Assets/scripts/Spawner.cs
+++ round3/Assets/scripts/Spawner.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Spawner : MonoBehaviour {
     private float rndNbr;
+    private float scaleMultiplier = 5f;
+    public int numSpawn;
     public int minNum;
     public int maxNum;    
     public Transform[] prefab;
@@ -10,12 +12,25 @@ public class Spawner : MonoBehaviour {
 
     public void Spawn()
     {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < numSpawn/3; i++)
         {
             float rndX, rndZ;
 
-            rndX = Random.Range(-plane.transform.localScale.x, plane.transform.localScale.x);
-            rndZ = Random.Range(-plane.transform.localScale.z, plane.transform.localScale.z);
+            rndX = Random.Range(-plane.transform.localScale.x * scaleMultiplier, plane.transform.localScale.x * scaleMultiplier);
+            rndZ = Random.Range(-plane.transform.localScale.z * scaleMultiplier, plane.transform.localScale.z * scaleMultiplier);
+
+            Object.Instantiate(prefab[0], new Vector3(rndX, 2, rndZ), transform.rotation);
+        }
+
+        for (int i = 0; i < numSpawn; i++)
+        {
+            float rndX, rndZ;
+            print(-plane.transform.localScale.x);
+            print(plane.transform.localScale.x);
+            rndX = Random.Range(-plane.transform.localScale.x * scaleMultiplier, plane.transform.localScale.x * scaleMultiplier);
+            rndZ = Random.Range(-plane.transform.localScale.z * scaleMultiplier, plane.transform.localScale.z * scaleMultiplier);
+            print(rndX);
+            print(rndZ);
 
             rndNbr = Random.Range(minNum, maxNum);
             Object.Instantiate(prefab[(int)rndNbr], new Vector3(rndX,2,rndZ), transform.rotation);
